5364476f2e48617ee40e2bc8136d49017f6ef915
Hassan <se7s.mohamed26@gmail.com>
2019-05-21 19:07:56 +0200

    .

--- server_dev.js
+++ server_dev.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const connString = require('./config')
+const connString = process.env.connString || require('./config')
 
 
 const quote = require('./routes/api/routes')
