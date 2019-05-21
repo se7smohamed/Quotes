912db60e1d8f36eeab7cf5dbdb4ea7f71e537dd4
Hassan <se7s.mohamed26@gmail.com>
2019-05-21 14:23:58 +0200

    modified:   server.js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const connString = require('./config')
-const cors = require("cors")
+const serverless = require('serverless-http');
+const cors = require("cors");
 
 
 const quote = require('./routes/api/routes')
@@ -19,5 +20,4 @@ mongoose
 
 
 app.use('/api/quote/', quote)
-const PORT = process.env.PORT || 5000
-app.listen(PORT, ()=>console.log(`listening on port ${PORT}`))
+module.exports = serverless(app)
